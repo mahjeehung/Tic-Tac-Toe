f3443b84c76f226ebf603b3b7d827e5bafd0d9e0
mahjeehung <151704278+mahjeehung@users.noreply.github.com>
2024-09-25 21:18:16 +0800

    Finish checkForWinner

--- script.js
+++ script.js
@@ -6,49 +6,59 @@ function createGameState(){
     return {gameState, Mark}
 }
 
-function createPlayer(name){
+function createPlayer(playerName){
     const playerToken =[];
     // ^array or plain value?
-    const addToken = (positionX, positionY) => playerToken.push(positionX,positionY);
+    const addToken = (positionX, positionY, positionAbsolute) => playerToken.push(positionX,positionY, positionAbsolute);
 
-    return {name, playerToken, addToken}
+    return {playerName, playerToken, addToken}
+    // do i need to return name?
 }
 
-function createBtn(btnNumber, positionX, positionY){
+function createBtn(coordinate, positionX, positionY, positionAbsolute){
     function playerMove(player) {
-        TicTacToe.Mark(player.name, btnNumber);
-        player.addToken(positionX, positionY);
+        TicTacToe.Mark(player.playerName, coordinate);
+        player.addToken(positionX, positionY, positionAbsolute);
         // gameOver fx
     }
     
-    return {positionX, positionY, playerMove};
-    
+    return {coordinate, positionX, positionY, positionAbsolute, playerMove};
+        // do i need to return coordinate, positionX, positionY, positionAbsolute?
 }
 
 
-// ------------------
 
-function mostFrequent(player) {
-  return Array.from(new Set(player.playerToken)).reduce((prev, curr) =>
-  player.playerToken.filter(el => el === curr).length > player.playerToken.filter(el => el === prev).length ? curr : prev
-);
+
+        function mostFrequent(player) {
+        return Array.from(new Set(player.playerToken)).reduce((prev, curr) =>
+        player.playerToken.filter(el => el === curr).length > player.playerToken.filter(el => el === prev).length ? curr : prev
+        );
+        }
+        const diagonal1 = ['UL','MM', 'BR'];
+        const diagonal2 = ['UR', 'MM', 'BL'];
+        function diagonalWinCon(player, pattern) {return pattern.every(i => player.playerToken.includes(i))};
+function checkForWinner(player){
+    if (player.playerToken.filter((word) => word==mostFrequent(player)).length > 2 ||diagonalWinCon(player, diagonal1)||diagonalWinCon(player, diagonal2)){return 'winner!'}
+    return 'no winner yet'
 }
 
-const diagonal1 = ['UL','MM', 'BR'];
-const diagonal2 = ['UR', 'MM', 'BL'];
-function diagonalWinCon(player, pattern) {return pattern.every(i => player.playerToken.includes(i))};
-
-if (player.playerToken.filter((word) => word==mostFrequent(player)).length > 2 ||diagonalWinCon(player, diagonal1)||diagonalWinCon(player, diagonal2)){return 'winner!'}
-let arr = ['L', 'U', 'L', 'L', 'U', 'L', 'LU', 'UL']
-
-// ------------------
-
+// const player1 = createPlayer('x');
+// player1.addToken('L','U','UL');
+// player1.addToken('L','U','MM');
+// player1.addToken('L','U','BR');
+// console.log(player1.playerToken);
+// console.log(checkForWinner(player1));
+// checkForWinner tests
 
 
 
-const TicTacToe = createGameState();
-const player1 = createPlayer('x');
-const btn0 = createBtn('0', 'L', 'U');
-btn0.playerMove(player1);
-console.log(TicTacToe.gameState);
-console.log(player1); 
+
+
+
+// const TicTacToe = createGameState();
+// const player1 = createPlayer('x');
+// const btn0 = createBtn('0', 'L', 'U');
+// btn0.playerMove(player1);
+// console.log(TicTacToe.gameState);
+// console.log(player1); 
+// tests
